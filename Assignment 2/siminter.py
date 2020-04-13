5b3d7f1cd64bdd831f282e5fdbdbff469629c7d0
proxy707 <princegaur1998@gmail.com>
2020-04-13 20:08:17 +0530

    file changed

--- Assignment 2/siminter.py
+++ Assignment 2/siminter.py
@@ -1,6 +1,7 @@
 def simpleinterest(p,r,t):
-    s=p(1.0+r*t)
+    s=p+p*r*t
     return s
 while True:
     p,r,t=map(float,input("Enter p,r,t:    ").split())
     print(simpleinterest(p,r,t))
+    print(p(1.0+r*t))
