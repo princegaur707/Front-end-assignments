159a76733fc3271dad506f3d99681a90b64bd3ac
proxy707 <princegaur1998@gmail.com>
2020-04-14 12:09:40 +0530

    file changed

--- Assignment 2/cubesum.py
+++ Assignment 2/cubesum.py
@@ -1,9 +1,8 @@
 def cubesum(n):
     s=0
     for i in range(1,n+1):
-        s+=i*i*i 
+        s+=i**3
     return s
 while True:
     n=int(input("Enter number:    "))
     print(cubesum(n))
-    
--- Assignment 2/fibcheck.py
+++ Assignment 2/fibcheck.py
@@ -1,11 +1,20 @@
-def fibonacci(n):
-    if(n==0):
-        return(0)
-    elif(n==1):
-        return(1)
-    else:
-        return(fibonacci(n-1)+fibonacci(n-2))
+#fibonacci series till 20th : 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181
+from math import ceil,sqrt
+fib={}
+def chkfibonacci(n):
+    def builddict(n):
+        for i in range(2,ceil(sqrt(n))):
+            if(i<=0):
+                return 0
+            if(i==1):
+                return 1
+            else:
+                fib[i]=builddict(n-1)+builddict(n-2)
+    builddict(n)
+    print(fib)
+    if n in fib.values():
+        return True
+    return False 
 while(True):
-    n=int(input("\nEnter the number: "))
-    for i in range(n+1):
-        print(fibonacci(i), end=" ")
+    n=int(input("\nEnter the number to check: "))
+    print(chkfibonacci(n))
