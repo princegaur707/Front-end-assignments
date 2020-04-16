80f8522909a5fb0bf4ef39dc458ab916ecb117e6
proxy707 <princegaur1998@gmail.com>
2020-04-16 21:06:50 +0530

    file added

--- Assignment 3/sortbylast.py
+++ Assignment 3/sortbylast.py
@@ -1,7 +1,5 @@
 while True:
-    nums=[]
-    n=int(input())
-    for i in range(n):
-        nums.append(input())
-    print(nums)
-    print(type(nums[0]))
+    t = list(tuple(map(int,input().split())) for r in range(int(input('Enter Rows : ')))) 
+    print(f"Original Tuple:  {t}")
+    t.sort(key=lambda x:[x[1],x[0]])   #IF the second element of tuple will be same then first element will be considered
+    print(f"Sorted Tuple:     {t}")
