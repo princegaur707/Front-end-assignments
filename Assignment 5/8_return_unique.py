46c29ba7c954d241b48a105ae94d71723f83c83f
proxy707 <princegaur1998@gmail.com>
2020-04-30 20:26:41 +0530

    file added

--- Assignment 5/8_return_unique.py
+++ Assignment 5/8_return_unique.py
@@ -1,5 +1,5 @@
 def returnunique(nums):
     return list(set(nums))
 while True:
-    nums=list(map(int,input("Enter list elements:     ").split()))
+    nums=list(map(int,input("Enter list elements:     ").split(',')))
     print(returnunique(nums))
